0722131003270318dc0637e39ffcc82389bd29b2
jerpelea <jerpelea@gmail.com>
2011-09-02 12:06:58 +0300

    semc:cleanup

--- build-bootimg-X10Mini/makeit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ramdisk
-find . | cpio -o -H newc | gzip > ../ramdisk.img
-cd ..
-./mkbootimg --cmdline 'androidboot.hardware=delta vmalloc=215M g_android.product_id=0x3149 console=ttyMSM0 semcandroidboot.startup=0x00000078' --kernel zImage --ramdisk ramdisk.img --base 0xd1800000 -o boot.img
